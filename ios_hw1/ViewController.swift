2f274ae0f1f0dd17548365ed8163bcff93a08ea4
almyk <hansentommy92@gmail.com>
2020-01-31 19:28:24 +0900

    Initial Commit

---
+++ ios_hw1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios_hw1
+//
+//  Created by Tommy Hansen on 2020/01/31.
+//  Copyright © 2020 Tommy Hansen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
